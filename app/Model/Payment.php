c0c5c7a388d69a85c2a3987d48ac3cf08991f130
SirPhemmiey <oluwafemiakinde>
2017-05-12 11:46:13 +0100

    adding files

--- app/Model/Payment.php
+++ app/Model/Payment.php
@@ -11,7 +11,7 @@ class Payment extends AppModel
       'foreignKey' => 'users_id')
       // 'foreignKey' => 'users_id')
     ); 
-  public function beforeSave($options = Array()){
+  public function beforeSave($options = array()){
       $this->data[$this->alias]['paid_date'] = date("Y-m-d H:i:s");
       return true;
   }
